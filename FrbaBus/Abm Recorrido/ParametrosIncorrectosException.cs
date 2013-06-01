8e3181fd0807cfce445978107ebcaa7ccb38ba93
FedericoGarou <lucascostasutn@gmail.com>
2013-06-01 13:21:35 -0700

    Terminado Alta recorrido.

---
+++ FrbaBus/Abm Recorrido/ParametrosIncorrectosException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FrbaBus.Abm_Recorrido
+{
+    class ParametrosIncorrectosException : System.Exception
+    {
+        public ParametrosIncorrectosException(String mensaje) : base(mensaje) {}
+    }
+}
